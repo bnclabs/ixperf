167af5ce3db0af6d4923210748a51a69ac045365
prataprc <prataprc@gmail.com>
2019-03-02 16:00:27 +0530

    README.

---
+++ README.rs
@@ -0,0 +1,6 @@
+**Performance measurement for [llrb-index][llrbindex] instances**
+
+* [x] Latency of a operations in min, max, average and in percentile.
+* [x] Throughput of operations.
+
+[llrbindex]: http://github.com/bnclabs/llrb-index
