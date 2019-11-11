ec9ef3af85a91b80ce8f81aa2f290acf2c75ff4d
prataprc <prataprc@gmail.com>
2019-11-11 16:54:45 +0530

    fix plot, include time-series for x-axis

--- src/plot.rs
+++ src/plot.rs
@@ -1,10 +1,11 @@
 use std::{
-    fs,
+    cmp, fs,
     io::{self, Read, Seek},
     path,
     str::FromStr,
 };
 
+use chrono::DateTime;
 use log::info;
 use plotters::{
     chart::SeriesLabelPosition,
@@ -17,13 +18,60 @@ use regex::Regex;
 use crate::Opt;
 
 struct PlotData {
-    title_initial: Vec<toml::Value>,
-    title_incrmnt: Vec<toml::Value>,
-    title_writers: Vec<toml::Value>,
-    title_readers: Vec<toml::Value>,
+    title_initial: Vec<(u64, toml::Value)>,
+    title_incrmnt: Vec<(u64, toml::Value)>,
+    title_writers: Vec<Vec<(u64, toml::Value)>>,
+    title_readers: Vec<Vec<(u64, toml::Value)>>,
 }
 
 impl PlotData {
+    fn normalize_time_axis(&mut self) {
+        if self.title_initial.len() > 0 {
+            let min = self.title_initial[0].clone().0;
+            self.title_initial = self
+                .title_initial
+                .clone()
+                .into_iter()
+                .map(|x| (x.0 - min, x.1))
+                .collect();
+        }
+        if self.title_incrmnt.len() > 0 {
+            let min = self.title_incrmnt[0].clone().0;
+            self.title_incrmnt = self
+                .title_incrmnt
+                .clone()
+                .into_iter()
+                .map(|x| (x.0 - min, x.1))
+                .collect();
+        }
+
+        let a = self
+            .title_writers
+            .iter()
+            .map(|xs| xs.iter().map(|x| x.0).min().unwrap_or(std::u64::MAX))
+            .min()
+            .unwrap_or(std::u64::MAX);
+        let b = self
+            .title_readers
+            .iter()
+            .map(|xs| xs.iter().map(|x| x.0).min().unwrap_or(std::u64::MAX))
+            .min()
+            .unwrap_or(std::u64::MAX);
+        let min = cmp::min(a, b);
+        self.title_writers = self
+            .title_writers
+            .clone()
+            .into_iter()
+            .map(|xs| xs.into_iter().map(|x| (x.0 - min, x.1)).collect())
+            .collect();
+        self.title_readers = self
+            .title_readers
+            .clone()
+            .into_iter()
+            .map(|xs| xs.into_iter().map(|x| (x.0 - min, x.1)).collect())
+            .collect();
+    }
+
     fn render(&self, opt: &Opt) {
         let path_dir = {
             let mut p = path::PathBuf::new();
@@ -38,11 +86,11 @@ impl PlotData {
         let y_axis1 = "Throughput kilo-ops / Sec";
         let y_axis2 = "Latency in nS";
         let lat = opt.percentile.as_str();
-        let plots = [
+        let mut plots = [
             (
                 "initial-load-throughput.png",
                 "initial-load throughput",
-                ["load"].to_vec(),
+                ["load".to_string()].to_vec(),
                 x_axis,
                 y_axis1,
                 [Self::get_ops("load", &self.title_initial)].to_vec(),
@@ -50,7 +98,7 @@ impl PlotData {
             (
                 "initial-load-latency.png",
                 "initial-load latency 98th percentile",
-                ["load"].to_vec(),
+                ["load".to_string()].to_vec(),
                 x_axis,
                 y_axis2,
                 [Self::get_lat_at("load", &self.title_initial, lat)].to_vec(),
@@ -58,7 +106,7 @@ impl PlotData {
             (
                 "incremental-ops-throughput.png",
                 "incremental-ops throughput",
-                ["set", "delete", "get"].to_vec(),
+                ["set".to_string(), "delete".to_string(), "get".to_string()].to_vec(),
                 x_axis,
                 y_axis1,
                 [
@@ -71,7 +119,7 @@ impl PlotData {
             (
                 "incremental-ops-latency.png",
                 "incremental-ops latency 98th percentile",
-                ["set", "delete", "get"].to_vec(),
+                ["set".to_string(), "delete".to_string(), "get".to_string()].to_vec(),
                 x_axis,
                 y_axis2,
                 [
@@ -84,7 +132,13 @@ impl PlotData {
             (
                 "incremental-range-throughput.png",
                 "incremental-range throughput",
-                ["range", "reverse", "range-items", "reverse-items"].to_vec(),
+                [
+                    "range".to_string(),
+                    "reverse".to_string(),
+                    "range-items".to_string(),
+                    "reverse-items".to_string(),
+                ]
+                .to_vec(),
                 x_axis,
                 y_axis1,
                 [
@@ -98,7 +152,7 @@ impl PlotData {
             (
                 "incremental-range-latency.png",
                 "incremental-range latency 98th percentile",
-                ["range", "reverse"].to_vec(),
+                ["range".to_string(), "reverse".to_string()].to_vec(),
                 x_axis,
                 y_axis2,
                 [
@@ -107,33 +161,50 @@ impl PlotData {
                 ]
                 .to_vec(),
             ),
-            (
-                "concurrent-rw-ops-throughput.png",
-                "concurrent-rw ops throughput",
-                ["set", "delete", "get"].to_vec(),
-                x_axis,
-                y_axis1,
-                [
-                    Self::get_ops("set", &self.title_writers),
-                    Self::get_ops("delete", &self.title_writers),
-                    Self::get_ops("get", &self.title_readers),
-                ]
-                .to_vec(),
-            ),
-            (
-                "concurrent-rw-ops-latency.png",
-                "concurrent-rw ops latency 98th percentile",
-                ["set", "delete", "get"].to_vec(),
-                x_axis,
-                y_axis2,
-                [
-                    Self::get_lat_at("set", &self.title_writers, lat),
-                    Self::get_lat_at("delete", &self.title_writers, lat),
-                    Self::get_lat_at("get", &self.title_readers, lat),
-                ]
-                .to_vec(),
-            ),
-        ];
+        ]
+        .to_vec();
+
+        let mut valuess = vec![];
+        let mut names = vec![];
+        for (i, writer) in self.title_writers.iter().enumerate() {
+            names.push(format!("writer-{}-set", i));
+            valuess.push(Self::get_ops("set", writer));
+            names.push(format!("writer-{}-delete", i));
+            valuess.push(Self::get_ops("delete", writer));
+        }
+        for (i, reader) in self.title_readers.iter().enumerate() {
+            names.push(format!("reader-{}-get", i));
+            valuess.push(Self::get_ops("get", &reader));
+        }
+        plots.push((
+            "concurrent-rw-ops-throughput.png",
+            "concurrent-rw ops throughput",
+            names,
+            x_axis,
+            y_axis1,
+            valuess,
+        ));
+
+        let mut valuess = vec![];
+        let mut names = vec![];
+        for (i, writer) in self.title_writers.iter().enumerate() {
+            names.push(format!("writer-{}-set", i));
+            valuess.push(Self::get_lat_at("set", &writer, lat));
+            names.push(format!("writer-{}-delete", i));
+            valuess.push(Self::get_lat_at("delete", &writer, lat));
+        }
+        for (i, reader) in self.title_readers.iter().enumerate() {
+            names.push(format!("reader-{}-get", i));
+            valuess.push(Self::get_lat_at("get", &reader, lat));
+        }
+        plots.push((
+            "concurrent-rw-ops-latency.png",
+            "concurrent-rw ops latency 98th percentile",
+            names,
+            x_axis,
+            y_axis2,
+            valuess,
+        ));
 
         // incremental plot, throughput
         for arg in plots.into_iter() {
@@ -142,35 +213,35 @@ impl PlotData {
         }
     }
 
-    fn get_ops(op_name: &str, vs: &Vec<toml::Value>) -> Vec<u64> {
+    fn get_ops(op_name: &str, vs: &Vec<(u64, toml::Value)>) -> Vec<(u64, u64)> {
         let mut out = vec![];
-        for v in vs {
+        for (sec, v) in vs {
             if let Some(table) = v.as_table() {
                 if let Some(table) = table.get(op_name) {
                     let v = table["ops"].as_integer().unwrap();
-                    out.push((v as u64) / 1000);
+                    out.push((*sec, (v as u64) / 1000));
                 }
             }
         }
         out
     }
 
-    fn get_items(op_name: &str, vs: &Vec<toml::Value>) -> Vec<u64> {
+    fn get_items(op_name: &str, vs: &Vec<(u64, toml::Value)>) -> Vec<(u64, u64)> {
         let mut out = vec![];
-        for v in vs {
+        for (sec, v) in vs {
             if let Some(table) = v.as_table() {
                 if let Some(table) = table.get(op_name) {
                     let v = table["items"].as_integer().unwrap();
-                    out.push(v as u64);
+                    out.push((*sec, v as u64));
                 }
             }
         }
         out
     }
 
-    fn get_lat_at(op_name: &str, vs: &Vec<toml::Value>, lat: &str) -> Vec<u64> {
+    fn get_lat_at(op_name: &str, vs: &Vec<(u64, toml::Value)>, lat: &str) -> Vec<(u64, u64)> {
         let mut out = vec![];
-        for v in vs {
+        for (sec, v) in vs {
             let table = if let Some(table) = v.as_table() {
                 table
             } else {
@@ -182,7 +253,7 @@ impl PlotData {
                 continue;
             };
             let t = t["latency"]["latencies"].as_table().unwrap();
-            out.push(t[lat].as_integer().unwrap() as u64);
+            out.push((*sec, t[lat].as_integer().unwrap() as u64));
         }
         out
     }
@@ -191,10 +262,10 @@ impl PlotData {
 fn do_render(
     file: &path::PathBuf,
     title: &str,
-    names: &Vec<&str>,
+    names: &Vec<String>,
     x_desc: &str,
     y_desc: &str,
-    valuess: &Vec<Vec<u64>>,
+    valuess: &Vec<Vec<(u64, u64)>>,
 ) {
     info!(target: "plot", "plotting throughput for {} at {:?}", title, file);
 
@@ -207,6 +278,9 @@ fn do_render(
         "reverse" => CYAN,
         "range-items" => MAGENTA,
         "reverse-items" => CYAN,
+        name if name.len() > 6 && name.contains("set") => GREEN,
+        name if name.len() > 6 && name.contains("delete") => RED,
+        name if name.len() > 6 && name.contains("get") => BLACK,
         name => panic!("unreachable {}", name),
     };
     let clrs: Vec<RGBColor> = names.iter().map(|n| color_for(n)).collect();
@@ -219,12 +293,16 @@ fn do_render(
     let root = BitMapBackend::new(&file, (1024, 768)).into_drawing_area();
     root.fill(&WHITE).expect("root file");
 
-    let x_maxes: Vec<usize> = valuess.iter().map(|values| values.len()).collect();
+    let x_maxes: Vec<u64> = valuess
+        .clone()
+        .into_iter()
+        .map(|values| values.into_iter().map(|x| x.0).max().unwrap_or(0))
+        .collect();
     let (xmin, xmax) = (0_u64, x_maxes.into_iter().max().unwrap_or(0) as u64);
     let y_maxes: Vec<u64> = valuess
         .clone()
         .into_iter()
-        .map(|v| v.into_iter().max().unwrap_or(0))
+        .map(|values| values.into_iter().map(|x| x.1).max().unwrap_or(0))
         .collect();
     let (ymin, ymax) = (0_u64, y_maxes.into_iter().max().unwrap_or(0));
     let ymax = ymax + (ymax / 5);
@@ -250,10 +328,7 @@ fn do_render(
         let clr2 = RGBColor(x, y, z);
         chart
             .draw_series(LineSeries::new(
-                values
-                    .iter()
-                    .enumerate()
-                    .map(|(j, value)| (j as u64, *value)),
+                values.iter().map(|(sec, value)| (*sec, *value)),
                 &clr1,
             ))
             .expect("draw series")
@@ -333,7 +408,8 @@ impl FromStr for PlotOps {
 }
 
 pub fn do_plot(opt: Opt) -> Result<(), String> {
-    let data = parse_log(&opt)?;
+    let mut data = parse_log(&opt)?;
+    data.normalize_time_axis();
     data.render(&opt);
     Ok(())
 }
@@ -347,36 +423,44 @@ fn parse_log(opt: &Opt) -> Result<PlotData, String> {
 
     let re1 = Regex::new(r"\[[0-9]{4}[^\]]*\].*").unwrap();
 
-    let mut title_initial: Vec<toml::Value> = vec![];
-    let mut title_incrmnt: Vec<toml::Value> = vec![];
-    let mut title_writers: Vec<toml::Value> = vec![];
-    let mut title_readers: Vec<toml::Value> = vec![];
-    // TODO: make this 128 element array
-    let mut writers: Vec<Vec<toml::Value>> = vec![];
-    let mut readers: Vec<Vec<toml::Value>> = vec![];
+    let mut title_initial: Vec<(u64, toml::Value)> = vec![];
+    let mut title_incrmnt: Vec<(u64, toml::Value)> = vec![];
+    let mut title_writers: Vec<Vec<(u64, toml::Value)>> = vec![];
+    let mut title_readers: Vec<Vec<(u64, toml::Value)>> = vec![];
 
-    let merge_thread = |ts: &mut Vec<Vec<toml::Value>>| -> Option<toml::Value> {
-        let mut vs: Vec<toml::Value> = ts
-            .iter_mut()
-            .filter_map(|t| if t.len() > 0 { Some(t.remove(0)) } else { None })
-            .collect();
-        match vs.len() {
-            0 => None,
-            1 => Some(vs.remove(0)),
-            _ => Some(
-                vs[1..]
-                    .iter()
-                    .fold(vs[0].clone(), |a, v| merge_toml(a, v.clone())),
-            ),
-        }
-    };
-
-    let try_merge_thread = |ts: &mut Vec<Vec<toml::Value>>| -> Option<toml::Value> {
-        match ts.iter().any(|t| t.len() == 0) {
-            true => None,
-            false => merge_thread(ts),
-        }
-    };
+    //let merge_thread = |ts: &mut Vec<Vec<toml::Value>>| -> Option<toml::Value> {
+    //    let mut vs: Vec<toml::Value> = ts
+    //        .iter_mut()
+    //        .filter_map(|t| if t.len() > 0 { Some(t.remove(0)) } else { None })
+    //        .collect();
+    //    match vs.len() {
+    //        0 => None,
+    //        1 => Some(vs.remove(0)),
+    //        _ => Some(
+    //            vs[1..]
+    //                .iter()
+    //                .fold(vs[0].clone(), |a, v| merge_toml(a, v.clone())),
+    //        ),
+    //    }
+    //};
+    //let try_merge_thread = |ts: &mut Vec<Vec<toml::Value>>| -> Option<toml::Value> {
+    //    match ts.iter().any(|t| t.len() == 0) {
+    //        true => None,
+    //        false => merge_thread(ts),
+    //    }
+    //};
+    //loop {
+    //    match merge_thread(&mut title_writers) {
+    //        None => break,
+    //        Some(v) => title_writers.push(v),
+    //    }
+    //}
+    //loop {
+    //    match merge_thread(&mut title_readers) {
+    //        None => break,
+    //        Some(v) => title_readers.push(v),
+    //    }
+    //}
 
     for mut file in opt.plot.0.iter() {
         let mut buf = vec![];
@@ -395,47 +479,29 @@ fn parse_log(opt: &Opt) -> Result<PlotData, String> {
             }
         }
 
-        let items: Vec<(String, usize, toml::Value)> = log_msgs
+        let items: Vec<(u64, String, usize, toml::Value)> = log_msgs
             .into_iter()
             .filter_map(|msg| parse_periodic_stats(msg))
             .collect();
         let max_writers = items
             .iter()
-            .filter_map(|x| if x.0 == "writer" { Some(x.1) } else { None })
+            .filter_map(|x| if x.1 == "writer" { Some(x.2) } else { None })
             .max()
             .unwrap_or(0);
-        writers.resize(max_writers + 1, vec![]);
+        title_writers.resize(max_writers + 1, vec![]);
         let max_readers = items
             .iter()
-            .filter_map(|x| if x.0 == "reader" { Some(x.1) } else { None })
+            .filter_map(|x| if x.1 == "reader" { Some(x.2) } else { None })
             .max()
             .unwrap_or(0);
-        readers.resize(max_readers + 1, vec![]);
-        for (title, thread, value) in items.into_iter() {
+        title_readers.resize(max_readers + 1, vec![]);
+        for (second, title, thread, value) in items.into_iter() {
             match title.as_str() {
-                "initial" => title_initial.push(value),
-                "incremental" => title_incrmnt.push(value),
-                title if &title[..6] == "writer" => {
-                    writers[thread].push(value);
-                    try_merge_thread(&mut writers).map(|v| title_writers.push(v));
-                }
-                title if &title[..6] == "reader" => {
-                    readers[thread].push(value);
-                    try_merge_thread(&mut readers).map(|v| title_readers.push(v));
-                }
+                "initial" => title_initial.push((second, value)),
+                "incremental" => title_incrmnt.push((second, value)),
+                title if &title[..6] == "writer" => title_writers[thread].push((second, value)),
+                title if &title[..6] == "reader" => title_readers[thread].push((second, value)),
                 _ => unreachable!(),
-            }
-        }
-        loop {
-            match merge_thread(&mut writers) {
-                None => break,
-                Some(v) => title_writers.push(v),
-            }
-        }
-        loop {
-            match merge_thread(&mut readers) {
-                None => break,
-                Some(v) => title_readers.push(v),
             }
         }
     }
@@ -467,24 +533,26 @@ fn merge_toml(one: toml::Value, two: toml::Value) -> toml::Value {
     }
 }
 
-fn parse_periodic_stats(msg: String) -> Option<(String, usize, toml::Value)> {
+fn parse_periodic_stats(msg: String) -> Option<(u64, String, usize, toml::Value)> {
     // TODO: move this into lazy_static
-    let re1 = Regex::new(r"\[.*\] (.+) periodic-stats.*").unwrap();
+    let re1 = Regex::new(r"\[([^ ]+) .*\] (.+) periodic-stats.*").unwrap();
     if !re1.is_match(&msg) {
         return None;
     }
 
     let lines: Vec<&str> = msg.lines().collect();
     let cap = re1.captures_iter(lines[0]).next().unwrap();
-    let tp: Vec<String> = cap[1].split("-").map(|x| x.to_string()).collect();
+    let tp: Vec<String> = cap[2].split("-").map(|x| x.to_string()).collect();
+    let dt = DateTime::parse_from_rfc3339(&cap[1]).unwrap();
+    let second = (dt.timestamp_millis() as f64 / 1000.0).round() as u64;
     let sstat = lines[1..].join("\n");
     let value: toml::Value = sstat.parse().expect("failed to parse stats");
     match tp[0].as_str() {
-        "initial" => Some((tp[0].clone(), 0, value)),
-        "incremental" => Some((tp[0].clone(), 0, value)),
+        "initial" => Some((second, tp[0].clone(), 0, value)),
+        "incremental" => Some((second, tp[0].clone(), 0, value)),
         "reader" | "writer" => {
             let thread = tp[1].parse().unwrap();
-            Some((tp[0].clone(), thread, value))
+            Some((second, tp[0].clone(), thread, value))
         }
         _ => unreachable!(),
     }
