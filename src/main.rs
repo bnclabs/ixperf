33c9c07755e8ce52131c5a38e652b29a006f670f
prataprc <prataprc@gmail.com>
2020-03-06 06:44:01 +0530

    cleanup.

--- src/main.rs
+++ src/main.rs
@@ -244,45 +244,5 @@ fn system_stats() {
     }
 }
 
-//fn system_stats() {
-//    use systemstat::{saturating_sub_bytes, Platform, System};
-//
-//    let sys = System::new();
-//    let mut cpu_load_aggr = sys.cpu_load_aggregate();
-//
-//    loop {
-//        thread::sleep(time::Duration::from_secs(1));
-//
-//        let mem_rss = match sys.memory() {
-//            Ok(mem) => saturating_sub_bytes(mem.total, mem.free),
-//            Err(err) => {
-//                error!("Memory: error: {}", err);
-//                std::process::exit(0);
-//            }
-//        };
-//        let cpu_load = match cpu_load_aggr {
-//            Ok(cpu) => {
-//                let cpu = cpu.done().unwrap();
-//                println!("cpu load {:?}", cpu);
-//                ((cpu.user * 100.0) as u64, (cpu.system * 100.0) as u64)
-//            }
-//            Err(err) => {
-//                error!("Memory: error: {}", err);
-//                std::process::exit(0);
-//            }
-//        };
-//
-//        let line = format!(
-//            "system = {{ cpu_user={} cpu_system={} mem_rss={} }}",
-//            cpu_load.0,
-//            cpu_load.1,
-//            mem_rss.as_u64()
-//        );
-//        info!(target: "ixperf", "sysmonitor periodic-stats\n{}", line);
-//
-//        cpu_load_aggr = sys.cpu_load_aggregate();
-//    }
-//}
-
 #[cfg(test)]
 mod jealloc_bench;
